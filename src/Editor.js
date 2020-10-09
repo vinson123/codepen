f71b94a886a0dbe80b144d4ea1dc8f7d0c30b909
vinson <vinson1609@gmail.com>
2020-10-09 07:44:29 +0530

    Codepen first commit.

---
+++ src/Editor.js
@@ -0,0 +1,44 @@
+import React, { useState }from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import {Controlled as ControlledEditor} from 'react-codemirror2'
+function Editor(props) {
+    const {
+        language,
+        displayName,
+        value,
+        onChange
+    } = props
+    const [open,setopen]= useState(true)
+
+ function handleChange(editor,data,value){
+   onChange(value)
+
+ }
+    return (
+        <div className={`editor ${open ? '' : 'collapsed'}`}>
+            <div className="editor-title">
+            {displayName}
+            <button onClick={()=>setopen(prevopen => !prevopen)}>-</button>
+            </div>
+            <ControlledEditor
+            onBeforeChange ={handleChange}
+            value = {value}
+            className = 'codemirror-wrapper'
+            options ={{
+                lineWrapping:true,
+                lint:true,
+                mode:language,
+                theme:'material',
+                lineNumbers:true
+            }}
+            />
+
+        </div>
+    )
+}
+
+export default Editor
